34ee3ea1e628d5c8899ecb5b70936808b0e7fd11
Nikita Skrynnik <nikita.skrynnik@xored.com>
2022-07-20 21:08:35 +1100

    change
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 	ctx, cancel := signal.NotifyContext(
 		context.Background(),
 		os.Interrupt,
-		// More Linux signals here
+		// More Linux sign als here
 		syscall.SIGHUP,
 		syscall.SIGTERM,
 		syscall.SIGQUIT,
